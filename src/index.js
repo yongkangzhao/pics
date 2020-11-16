e0566b69667c66830dd9aca0ae9a0180009c9075
Yongkang Zhao <cookieyong@gmail.com>
2020-11-15 17:28:53 -0800

    created a simple event handler for text input events

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import RactDOM from 'react-dom';
+import App from './components/Apps';
+
+
+reactDom.render(<App />, document.querySelector('#root'));

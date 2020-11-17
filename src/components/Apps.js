af4a17ef1f6288d055ea7d1e31c96af452367383
Yongkang Zhao <cookieyong@gmail.com>
2020-11-16 21:12:09 -0800

    implemented two versions of data fetching async/await and fetch/then to get image data via unsplash api

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App extends React.Component {
-    state = {};
-    onSearchSubmit(term){
-        console.log(term);
+    state = { images: [] };
+    // onSearchSubmit = async (term) => {
+    //     const res = await axios.get('https://api.unsplash.com/search/photos',{
+    //         params: { query: term},
+    //         headers: {
+    //             Authorization: 'Client-ID 0udcEDLLn3YoLKmgMfITljsJe54B7Y9eH1NaaI9m68M'
+    //         }
+    //     });
+    //     this.setState({images: res.data.results});
+    // }
+
+    onSearchSubmit = async (term) => {
+        await axios.get('https://api.unsplash.com/search/photos',{
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID 0udcEDLLn3YoLKmgMfITljsJe54B7Y9eH1NaaI9m68M'
+            }
+        }).then(res => {this.setState({images: res.data.results})});
     }
+
+
 
     render (){
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         )
     };

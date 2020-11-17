2b5258b111b6bd8e26af7c7a0beff0f10fe9739b
Yongkang Zhao <cookieyong@gmail.com>
2020-11-16 20:01:52 -0800

    updated the searchbox on submit to invode a parent method

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -3,16 +3,18 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
     state = {};
-
+    onSearchSubmit(term){
+        console.log(term);
+    }
 
     render (){
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
-                <SearchBar />
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         )
     };
-};
+}
 
 
 export default App;

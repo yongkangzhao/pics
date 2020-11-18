18ed5fb9c30a25f9639fa215af0db25c99b37398
Yongkang Zhao <cookieyong@gmail.com>
2020-11-17 19:12:49 -0800

    implemented dynamic grid row end span amount based on grid row amount and height of image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+        this.state= { spans : 0 };
+    }
+
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = (e) => {
+        // console.log(e.path[0].clientHeight);
+        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const spans = height;
+        this.setState({ spans });
+    }
+
+
     render () {
+        const {description, urls} = this.props.image;
         return (
-            <div>
-                
-                <img
-                alt={this.props.image.description} 
-                src ={this.props.image.urls.regular}
-                />
+            <div style={{gridRowEnd:`span ${this.state.spans}`}}>
+                <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         )
     }

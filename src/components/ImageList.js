544e6fe3bb50431b5fb42760639434be437c71ef
Yongkang Zhao <cookieyong@gmail.com>
2020-11-17 18:21:31 -0800

    created another component imagecard to handle creation of images for imagelist and another css for imagelist to display images in grid style

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,14 @@
+import './ImageList.css'
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-    console.log(props.images);
     
 
     const images = props.images.map(image=>{
-        return <img key={image.id} alt={image.description} src={image.urls.regular} />
+        return <ImageCard key={image.id} image={image}/>;
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
